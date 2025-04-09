e0be256eaf20bda728a99c693ece8a54968c82b7
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2025-04-09 09:32:40 +0200

    Automatic formatting with ruff

--- mibiremo/irmresult.py
+++ mibiremo/irmresult.py
@@ -4,14 +4,14 @@ def IRM_RESULT(code):
     See: IrmResult.h File Reference.
     """
     map = {
-        0:("IRM_OK","Success"),
-        -1:("IRM_OUTOFMEMORY","Failure, Out of memory"),
-        -2:("IRM_BADVARTYPE","Failure, Invalid VAR type"),
-        -3:("IRM_INVALIDARG","Failure, Invalid argument"),
-        -4:("IRM_INVALIDROW","Failure, Invalid row"),
-        -5:("IRM_INVALIDCOL","Failure, Invalid column"),
-        -6:("IRM_BADINSTANCE","Failure, Invalid rm instance id"),
-        -7:("IRM_FAIL","Failure, Unspecified"),
+        0: ("IRM_OK", "Success"),
+        -1: ("IRM_OUTOFMEMORY", "Failure, Out of memory"),
+        -2: ("IRM_BADVARTYPE", "Failure, Invalid VAR type"),
+        -3: ("IRM_INVALIDARG", "Failure, Invalid argument"),
+        -4: ("IRM_INVALIDROW", "Failure, Invalid row"),
+        -5: ("IRM_INVALIDCOL", "Failure, Invalid column"),
+        -6: ("IRM_BADINSTANCE", "Failure, Invalid rm instance id"),
+        -7: ("IRM_FAIL", "Failure, Unspecified"),
     }
 
     if code in map:
