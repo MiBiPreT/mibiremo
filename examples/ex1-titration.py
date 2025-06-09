c0f6aeb5e06071a17648fcf0cce14410fca60c54
Matteo Masi <4078033+matteomasi@users.noreply.github.com>
2025-06-09 18:50:35 +0200

    Updated structure, clean up, improved documentation

--- examples/ex1-titration.py
+++ examples/ex1-titration.py
@@ -1,176 +1,166 @@
-## MiBiReMo - Example - Calculate calcite titration curve
-# CaCO3(s) + 2HCl(aq) → CaCl2(aq) + CO2(g) + H2O(l)
-# Chemical reactions assumed at equilibrium
-#
-# Last revision: 02/07/2024
+"""
+MiBiReMo - Example - Calculate calcite titration curve.
 
-# %%
+This script models the reaction:
+CaCO3(s) + 2HCl(aq) → CaCl2(aq) + CO2(g) + H2O(l)
+assuming chemical reactions at equilibrium.
+
+Author: Matteo Masi
+Last revision: 09/06/2025
+"""
+
 import time
 import matplotlib.pyplot as plt
 import numpy as np
 import mibiremo
 
-# %%
-########### SETTINGS ##############
-db = "../mibiremo/database/phreeqc.dat"  # .dat database path
-ncells = 1000  # number of model cells
-nthreads = 4  # multithread calculation. -1 for all CPUs
-pqifile = "pqi/ex1_Calcite_titration.pqi"  # Name of the phreeqc input file
-HCl_range = [0.0, 4.0]  # mol/L
 
-unit_sol = 2  # 1: mg/L; 2: mol/L; 3: kg/kgs
+# Global settings
+database_path = "../mibiremo/database/phreeqc.dat"  # .dat database path
+n_cells = 1000  # Number of model cells
+n_threads = 4  # Multithread calculation (-1 for all CPUs)
+pqi_file = "pqi/ex1_Calcite_titration.pqi"  # Phreeqc input file
+hcl_range = [0.0, 4.0]  # mol/L
+
+# Unit settings
+unit_solution = 2  # 1: mg/L; 2: mol/L; 3: kg/kgs
 units = 1  # 0: mol/L cell; 1: mol/L water; 2: mol/L rock
 
-n = 1.0  # Porosity
-S = 1.0  # Saturation
-##################################
-
-# %%
-# Initialize Phreeqc
-phr = mibiremo.PhreeqcRM()
-phr.create(nxyz=ncells, n_threads=nthreads)
-
-# Load database
-status = phr.RM_LoadDatabase(db)
-
-# %%
-### Set properties/parameters
-phr.RM_SetComponentH2O(0)  # Don't include H2O in the component list
-phr.RM_SetRebalanceFraction(0.5)  # Rebalance the load of each thread
-
-### Set units
-phr.RM_SetUnitsSolution(unit_sol)
-phr.RM_SetUnitsPPassemblage(units)
-phr.RM_SetUnitsExchange(units)
-phr.RM_SetUnitsSurface(units)
-phr.RM_SetUnitsGasPhase(units)
-phr.RM_SetUnitsSSassemblage(units)
-phr.RM_SetUnitsKinetics(units)
-
-### Porosity and saturation
-phr.RM_SetPorosity(n * np.ones(ncells))
-phr.RM_SetSaturation(S * np.ones(ncells))
-
-### Create error log files
-phr.RM_SetFilePrefix("titr")
-phr.RM_OpenFiles()
-
-### Multicomponent diffusion transport calculation settings
-phr.RM_SetSpeciesSaveOn(1)
-
-# %%
-# RUN
-status = phr.RM_RunFile(1, 1, 1, pqifile)
-# Print some of the reaction module information and log to file
-th = phr.RM_GetThreadCount()
-str1 = f"Number of threads:  {th}\n"
-phr.RM_OutputMessage(str1)
-
-# %%
-# Transfer solutions and reactants from the InitialPhreeqc instance to the reaction-module workers
-# Column index for the initial conditions ic1.shape ->  (ncells, 7)
-# (1) SOLUTIONS, (2) EQUILIBRIUM_PHASES, (3) EXCHANGE, (4) SURFACE, (5) GAS_PHASE, (6) SOLID_SOLUTIONS, and (7) KINETICS
-
-ncl = phr.RM_GetGridCellCount()
-
-ic1 = -1 * np.ones(ncells * 7, dtype=np.int32)
-for i in np.arange(ncells):
-    ic1[i] = 1  # Solution 1
-    ic1[i + ncells] = 1  # Equilibrium phases 1
-    ic1[i + 2 * ncells] = -1  # Exchange none
-    ic1[i + 3 * ncells] = -1  # Surface none
-    ic1[i + 4 * ncells] = -1  # Gas phase none
-    ic1[i + 5 * ncells] = -1  # Solid solutions none
-    ic1[i + 6 * ncells] = -1  # Kinetics none
-
-ic2 = -1 * np.ones(ncells * 7, dtype=np.int32)
-f1 = np.ones(ncells * 7, dtype=np.float64)
-
-# %%
-status = phr.RM_InitialPhreeqc2Module(ic1, ic2, f1)
-
-# Get number and name of components and species
-ncomps = phr.RM_FindComponents()
-nspecies = phr.RM_GetSpeciesCount()
-
-# Get the components
-components = np.zeros(ncomps, dtype="U20")
-for i in range(ncomps):
-    status = phr.RM_GetComponent(i, components, 20)
-
-# Get the species
-species = np.zeros(nspecies, dtype="U20")
-for i in range(nspecies):
-    status = phr.RM_GetSpeciesName(i, species, 20)
+# Physical properties
+porosity = 1.0  # Porosity
+saturation = 1.0  # Saturation
 
 
-# %%
-# Retrieve initial solution
-# Make an initial step (zero lenght) to be sure that
-# everything is at equilibrium
-phr.RM_SetTime(0.0)
-phr.RM_SetTimeStep(0.0)
-status = phr.RM_RunCells()
+def initialize_phreeqc():
+    """Initialize and configure PhreeqcRM instance."""
+    phr = mibiremo.PhreeqcRM()
+    phr.create(nxyz=n_cells, n_threads=n_threads)
 
-# %%
-# Initialize component concentration Cc vector
-Cc = np.zeros(ncells * ncomps, dtype=np.float64)
-status = phr.RM_GetConcentrations(Cc)
+    # Load database
+    status = phr.RM_LoadDatabase(database_path)
+    if status != 0:
+        raise RuntimeError("Failed to load Phreeqc database")
 
-# Initialize species concentration Cs matrix
-Cs = np.zeros(ncells * nspecies, dtype=np.float64)
-status = phr.RM_GetSpeciesConcentrations(Cs)
+    # Set properties/parameters
+    phr.RM_SetComponentH2O(0)  # Don't include H2O in component list
+    phr.RM_SetRebalanceFraction(0.5)  # Rebalance thread load
 
-# Get species diffusion coefficients
-# D = np.zeros(nspecies,dtype=np.float64)
-# status = phr.RM_GetSpeciesD25(D)
+    # Set units
+    phr.RM_SetUnitsSolution(unit_solution)
+    phr.RM_SetUnitsPPassemblage(units)
+    phr.RM_SetUnitsExchange(units)
+    phr.RM_SetUnitsSurface(units)
+    phr.RM_SetUnitsGasPhase(units)
+    phr.RM_SetUnitsSSassemblage(units)
+    phr.RM_SetUnitsKinetics(units)
 
-# %%
-# GET SELECTED OUTPUT
-nselout = phr.RM_GetSelectedOutputCount()
-ncolsel = phr.RM_GetSelectedOutputColumnCount()
-selout = np.zeros(ncells * ncolsel, dtype=np.float64)
-status = phr.RM_GetSelectedOutput(selout)
+    # Set porosity and saturation
+    phr.RM_SetPorosity(porosity * np.ones(n_cells))
+    phr.RM_SetSaturation(saturation * np.ones(n_cells))
 
-# Get headers of the selected output
-selout_h = np.zeros(ncolsel, dtype="U100")
-for i in range(ncolsel):
-    status = phr.RM_GetSelectedOutputHeading(i, selout_h, 100)
+    # Create error log files
+    phr.RM_SetFilePrefix("titr")
+    phr.RM_OpenFiles()
+
+    # Multicomponent diffusion settings
+    phr.RM_SetSpeciesSaveOn(1)
+
+    return phr
 
 
-# %%
-# SET SPECIES CONCENTRATION
-HCl = np.linspace(HCl_range[0], HCl_range[1], ncells)  # mol/L
+def run_initial(phr):
+    """Run initial setup and return component/species information."""
+    # Run the initial setup file
+    status = phr.RM_RunFile(1, 1, 1, pqi_file)
+    if status != 0:
+        raise RuntimeError("Failed to run Phreeqc input file")
 
-# Find column of Cl- and H+ species
-indx_Cl = np.where(species == "Cl-")[0][0]
-indx_H = np.where(species == "H+")[0][0]
+    # Log thread information
+    th = phr.RM_GetThreadCount()
+    phr.RM_OutputMessage(f"Number of threads: {th}\n")
 
-# Species matrix
-Cs_r = Cs.reshape(nspecies, ncells).T
+    # Transfer initial conditions
+    ic1 = -1 * np.ones(n_cells * 7, dtype=np.int32)
+    for i in range(n_cells):
+        ic1[i] = 1  # Solution 1
+        ic1[i + n_cells] = 1  # Equilibrium phases 1
+        # Other phases set to none (-1)
+        ic1[i + 2*n_cells:i + 7*n_cells] = -1
 
-# %%
-# ADD HCl AND RUN
-Cs_r[:, indx_Cl] = Cs_r[:, indx_Cl] + HCl  # Cl-
-Cs_r[:, indx_H] = Cs_r[:, indx_H] + HCl  # H+
-Cs0 = Cs_r.T.reshape(ncells * nspecies)
-Cs1 = np.copy(Cs0)
-status = phr.RM_SpeciesConcentrations2Module(Cs1)
-phr.RM_SetTime(1.0)
-phr.RM_SetTimeStep(1.0)
+    ic2 = -1 * np.ones(n_cells * 7, dtype=np.int32)
+    f1 = np.ones(n_cells * 7, dtype=np.float64)
 
-t = time.time()
-phr.RM_RunCells()
-elapsed = time.time() - t
+    status = phr.RM_InitialPhreeqc2Module(ic1, ic2, f1)
 
-# %%
-# GET RESULTS - SELECTED OUTPUT
-s_pd = phr.pdSelectedOutput()  # Returns a pandas Data Frame
+    # Get component and species information
+    n_comps = phr.RM_FindComponents()
+    n_species = phr.RM_GetSpeciesCount()
 
-# %%
-# PLOTS
-plt.figure(figsize=(10, 6))
-plt.plot(HCl, s_pd.pH)
-plt.xlabel("molH$^+$ added")
-plt.ylabel("pH")
-plt.show()
+    components = np.zeros(n_comps, dtype="U20")
+    for i in range(n_comps):
+        phr.RM_GetComponent(i, components, 20)
+
+    species = np.zeros(n_species, dtype="U20")
+    for i in range(n_species):
+        phr.RM_GetSpeciesName(i, species, 20)
+
+    # Run initial equilibrium step
+    phr.RM_SetTime(0.0)
+    phr.RM_SetTimeStep(0.0)
+    status = phr.RM_RunCells()
+
+    return components, species
+
+
+def main():
+    """Main execution function for the calcite titration simulation."""
+    # Initialize Phreeqc and run the initial setup simulation
+    phr = initialize_phreeqc()
+    components, species = run_initial(phr)
+    n_comps = len(components) # Number of components
+    n_species = len(species) # Number of species
+
+    # Initialize concentration vectors
+    cc = np.zeros(n_cells * n_comps, dtype=np.float64)
+    cs = np.zeros(n_cells * n_species, dtype=np.float64)
+    phr.RM_GetConcentrations(cc)
+    phr.RM_GetSpeciesConcentrations(cs)
+
+    # Set HCl concentrations
+    hcl = np.linspace(hcl_range[0], hcl_range[1], n_cells)  # mol/L
+
+    # Find indices for Cl- and H+ species
+    indx_cl = np.where(species == "Cl-")[0][0]
+    indx_h = np.where(species == "H+")[0][0]
+
+    # Update species concentrations with HCl
+    cs_r = cs.reshape(n_species, n_cells).T
+    cs_r[:, indx_cl] += hcl  # Cl-
+    cs_r[:, indx_h] += hcl  # H+
+    cs1 = cs_r.T.reshape(n_cells * n_species)
+
+    # Run simulation with added HCl
+    phr.RM_SpeciesConcentrations2Module(cs1)
+    phr.RM_SetTime(1.0)
+    phr.RM_SetTimeStep(1.0)
+
+    start_time = time.time()
+    phr.RM_RunCells()
+    elapsed = time.time() - start_time
+    print(f"Simulation completed in {elapsed:.2f} seconds")
+
+    # Get results
+    results_df = phr.pdSelectedOutput()
+
+    # Plot results
+    plt.figure(figsize=(10, 6))
+    plt.plot(hcl, results_df['pH'])
+    plt.xlabel("mol H$^+$ added")
+    plt.ylabel("pH")
+    plt.title("Calcite Titration Curve")
+    plt.grid(True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
