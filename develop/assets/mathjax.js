c1d91ea94330326746a9b5d7948beb8bce31bb53
Docs Deploy <docs.deploy@example.co.uk>
2026-02-19 07:11:58 +0000

    Deployed e46f5d0 to develop with MkDocs 1.6.1 and mike 2.1.3

--- develop/assets/mathjax.js
+++ develop/assets/mathjax.js
@@ -1,22 +1,19 @@
 window.MathJax = {
-  loader: {load: ['[tex]/ams']},
-  tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
-    processEscapes: true,
-    processEnvironments: true,
-    packages: {'[+]': ['ams']}
-  },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
-  }
-};
-
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"]],
+      processEscapes: true,
+      processEnvironments: true
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
   
-document$.subscribe(() => { 
-  MathJax.startup.output.clearCache()
-  MathJax.typesetClear()
-  MathJax.texReset()
-  MathJax.typesetPromise()
-})
+  document$.subscribe(() => { 
+    MathJax.startup.output.clearCache()
+    MathJax.typesetClear()
+    MathJax.texReset()
+    MathJax.typesetPromise()
+  })
